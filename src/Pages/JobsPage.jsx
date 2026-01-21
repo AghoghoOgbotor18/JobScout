cf05609b7f2557094215b1f98f719f33c6aebc83
Aghogho Ogbotor <sarahogbotor27@gmail.com>
2026-01-22 00:21:15 +0100

    fix: handle undefined job data and empty search result

--- src/Pages/JobsPage.jsx
+++ src/Pages/JobsPage.jsx
@@ -8,23 +8,16 @@ const JobsPage = () => {
     const [search, setSearch] = useState("");
     const [page, setPage] = useState(1);
     const debouncedSearch = useDebounce(search, 800);
-    const {data: jobs, isLoading, error} = useJobs(debouncedSearch);
+    const {data: jobs = [], isLoading, isFetching, error} = useJobs(debouncedSearch);
 
     useEffect(() => {
         window.scrollTo(0,0);
     }, [page]);
 
-    if (isLoading) return (
-        <div className='flex items-center justify-center'>
-            <p className='w-20 h-20 border-4 border-green-800 rounded-full border-t-transparent animate-spin bg-transparent text-center my-10'></p>
-        </div>
-    )
     if (error) return <p className='text-red-700 text-md'>error loading jobs</p>
 
     const start = (page - 1) * jobsPerPage;
     const paginateJobs = jobs.slice(start, start + jobsPerPage);
-
-    
 
     return (
         <div className='bg-white mb-4'>
@@ -33,7 +26,22 @@ const JobsPage = () => {
                 <input type='search' value={search} placeholder="search jobs" onChange={(e) => {setSearch(e.target.value); setPage(1);}}  className='border border-white bg-green-200 p-2 rounded-md text-black/60 outline-0'/>
             </div>
 
+            {isLoading && (
+                <div className='flex items-center justify-center'>
+                    <p className='w-20 h-20 border-4 border-green-800 rounded-full border-t-transparent animate-spin bg-transparent text-center my-30'></p>
+                </div>
+            )}
+            {!isLoading && jobs.length === 0 && debouncedSearch && (
+                <div className="flex justify-center items-center mt-40">
+                    <h1 className="text-2xl font-bold">
+                    No job listings found for
+                    <span className="text-green-800"> "{debouncedSearch}"</span>
+                    </h1>
+                </div>
+                )}
+
             <div className='grid grid-cols-1 gap-7 py-3 px-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 container mx-auto mt-17'>
+
                 {paginateJobs.map(job => (
                     <div key={job.id} className='flex flex-col gap-4 border-2 border-green-800 rounded-md bg-green-50 px-4 py-3 hover:-translate-y-0.5 duration-75'>
                         <h2 className='font-black text-lg'>{job.title}</h2>
